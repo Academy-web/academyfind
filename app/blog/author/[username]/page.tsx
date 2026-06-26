67101d8173cc4303a10f3cec4367037f51db69d0
Monish <monishmathurt@gmail.com>
2026-06-26 16:17:24 +0530

    feat(blog): create author page to display author information and posts

---
+++ app/blog/author/[username]/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogAuthorPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Author Page</h1>
+      <p>This is the author page. You can display author information and their posts here.</p>
+    </div>
+  );
+}
